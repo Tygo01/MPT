b091b230967ca92471e265b847101ab6270440a7
Tygo01 <tygostrijker08@gmail.com>
2023-04-17 12:55:08 +0200

    Encoder now creates a wav file, not functional yet

--- src/encoder.py
+++ src/encoder.py
@@ -1,14 +1,36 @@
 from PIL import Image
+import numpy as np
+from scipy.io.wavfile import write
+
+# C:\Users\113977\Downloads\t.png
+# C:\Users\113977\Downloads\test.wav
 
 class Encoder:
     def encode(self, imageLocation, outputLocation):
         img = Image.open(imageLocation)
         img = img.convert("L")  # Convert the image to grayscale
 
-        pixel_values = list(img.getdata())
+        pixel_values = np.array(img.getdata())
         # Normalize the pixel values to be between 0 and 1
-        normalized_values = [x / 255.0 for x in pixel_values]
-        # Reshape the normalized pixel values into a 2D array
-        width, height = img.size
-        normalized_pixels = [normalized_values[i:i + width] for i in range(0, len(normalized_values), width)]
-        print(normalized_pixels)
+        normalized_pixels = (pixel_values + 1)/255.0
+
+        sample_rate = 44100
+        duration = 1
+        t = np.linspace(0, duration, int(duration * sample_rate), endpoint=False)
+        fr = []
+
+        for x in np.nditer(normalized_pixels):
+            fr.append(x * 8000)
+
+        frequencies = np.array(fr)
+
+        waves = np.zeros_like(t)
+        for freq in frequencies:
+            waves += 1.0 * np.sin(2 * np.pi * freq * t)
+
+        waves /= np.max(np.abs(waves))
+        waves = np.int16(waves * 32767)
+
+        print(waves)
+
+        write(outputLocation, sample_rate, waves)
